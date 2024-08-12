06a4e99a4b702b922486eb3b8dd0aed94a413469
Fernando Sinaga <fernandosinaga427@gmail.com>
2024-08-13 00:47:40 +0700

    Projek Button dua

--- app/src/main/java/com/example/projekbutton/MainActivity.java
+++ app/src/main/java/com/example/projekbutton/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.projekbutton;
-
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,19 +8,32 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
 public class MainActivity extends AppCompatActivity {
+    Button bubahwarna, btampilpesan;
+    TextView ttampil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //ini adalah perintah memanggil button di View
+        bubahwarna = (Button) findViewById(R.id.btnubahwarna);
+        btampilpesan = (Button) findViewById(R.id.btnpesan);
+        ttampil= (TextView) findViewById(R.id.txttampil);
+        //ini adalah perintah click untuk button pesan ubah warna
+        bubahwarna.setOnClickListener(new
     }
 }
